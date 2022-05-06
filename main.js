44f6703acc0aacc898f0259a436deb4544d6bc3b
sonph <sonph>
2022-05-06 10:01:01 +0700

    nopbai

---
+++ main.js
@@ -0,0 +1,10 @@
+let inputwidth;
+let inputheight;
+
+inputwidth = prompt("nhập chiều rộng")
+inputheight = prompt("nhập chiều dài")
+
+let width = parseInt(inputwidth);
+let height = parseInt(inputheight);
+ let dientich = width * height
+ document.write("diện tích hình chữ nhật là " + dientich);
